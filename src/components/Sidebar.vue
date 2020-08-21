691fcd07405582b024a8ab8b3890dbfe2540efb1
Noe Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2020-08-21 11:16:55 +0200

    sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-navigation-drawer
+      v-model="drawer"
+      app
+      permanent
+      expand-on-hover
+  >
+    <v-list dense>
+      <v-list-item link to="/" exact>
+        <v-list-item-action>
+          <v-icon>mdi-monitor-dashboard</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Dashboard</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link to="/about" exact>
+        <v-list-item-action>
+          <v-icon>mdi-cog</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Settings</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+    </v-list>
+    <template v-slot:append>
+      <v-list-item link to="/logout" exact>
+        <v-list-item-action>
+          <v-icon>mdi-amazon-alexa</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Logout</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </template>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  data: () => ({
+    drawer: null,
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import App from "@/App";
+import Dashboard from "@/components/Dashboard";
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: App
+    name: 'Dashboard',
+    component: Dashboard
   },
   {
     path: '/about',
@@ -16,7 +16,7 @@ Vue.use(VueRouter)
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '@/App')
+    component: () => import(/* webpackChunkName: "about" */ '@/components/Settings')
   }
 ]
 
