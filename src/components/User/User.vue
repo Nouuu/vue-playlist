e90333dcc063c9b6c882b070ee64b1c12f01f78b
Noe Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2020-08-26 17:14:43 +0200

    see user's lists

--- src/components/User/User.vue
+++ src/components/User/User.vue
@@ -1,21 +1,56 @@
 <template>
   <div class="row">
-    <v-card outlined elevation="6" class="col-xl-4 col-lg-6 col-md-8 col-12">
-      <v-card-title>
-        {{ email_user }}
-      </v-card-title>
-      <v-progress-linear
-          indeterminate
-          v-if="api_user_progress_bar"
-      ></v-progress-linear>
-      <v-card-text v-if="!api_user_progress_bar">
-        <ul>
-          <li>Pseudo : {{ user.pseudo_user }}</li>
-          <li>Role : {{ user.role_user }}</li>
-          <li>Date d'inscription : {{ user.date_inscription_user }}</li>
-        </ul>
-      </v-card-text>
-    </v-card>
+    <v-col cols="12" md="8" lg="6" xl="4">
+
+      <v-card outlined elevation="6">
+        <v-card-title>
+          {{ email_user }}
+        </v-card-title>
+        <v-progress-linear
+            indeterminate
+            v-if="api_user_progress_bar"
+        ></v-progress-linear>
+        <v-card-text v-if="!api_user_progress_bar">
+          <ul>
+            <li>Pseudo : {{ user.pseudo_user }}</li>
+            <li>Role : {{ user.role_user }}</li>
+            <li>Date d'inscription : {{ user.date_inscription_user }}</li>
+          </ul>
+        </v-card-text>
+      </v-card>
+    </v-col>
+
+    <v-col cols="12" md="8" lg="6" xl="6">
+      <v-card>
+        <v-card-title class="d-flex align-baseline">
+          Listes
+          <v-spacer></v-spacer>
+          <v-text-field
+              v-model="list_searchbar"
+              append-icon="mdi-magnify"
+              label="Rechercher"
+              single-line
+              hide-details>
+          </v-text-field>
+          <v-btn icon color="green"
+                 @click="api_get_user_playlist">
+            <v-icon>mdi-cached</v-icon>
+          </v-btn>
+
+        </v-card-title>
+        <v-progress-linear
+            indeterminate
+            v-if="api_list_progress_bar"
+        ></v-progress-linear>
+        <v-data-table
+            v-if="!api_list_progress_bar"
+            :headers="lists_header"
+            :items="lists"
+            :search="list_searchbar">
+        </v-data-table>
+      </v-card>
+    </v-col>
+
 
     <v-snackbar v-model="error_snackbar">
       Erreur API
@@ -38,6 +73,23 @@ export default {
     return {
       email_user: '',
       user: {},
+      lists: [],
+      lists_header: [
+        {
+          text: 'Nom de la liste',
+          align: 'start',
+          value: 'name_list'
+        },
+        {
+          text: 'Date de création',
+          value: 'date_creation_list'
+        },
+        {
+          text: 'Nombre d\'albums',
+          value: 'album_count'
+        }
+      ],
+      list_searchbar: '',
       api_user_progress_bar: false,
       api_list_progress_bar: false,
       error_snackbar: false
@@ -46,18 +98,32 @@ export default {
   mounted() {
     this.email_user = this.$route.params.email_user;
     this.api_get_user();
+    this.api_get_user_playlist();
   },
   methods: {
     api_get_user() {
       this.api_user_progress_bar = true;
       this.error_snackbar = false;
-      this.$http.get(this.$api_url + 'user/' + 'single_read.php?email=' + this.email_user)
+      this.$http.get(this.$api_url + 'user/single_read.php?email=' + this.email_user)
           .then(result => {
             this.user = result.data;
             this.api_user_progress_bar = false;
           })
           .catch(() => {
             this.api_user_progress_bar = false;
+            this.error_snackbar = true;
+          })
+    },
+    api_get_user_playlist() {
+      this.api_list_progress_bar = true;
+      this.error_snackbar = false;
+      this.$http.get(this.$api_url + 'list/read_user.php?email=' + this.email_user)
+          .then(result => {
+            this.lists = result.data.body;
+            this.api_list_progress_bar = false;
+          })
+          .catch(() => {
+            this.api_list_progress_bar = false;
             this.error_snackbar = true;
           })
     },
