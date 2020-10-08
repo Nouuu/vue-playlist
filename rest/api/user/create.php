cabc23d0e5a03771b321ceb099f4ca09dd14680e
Noe Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2020-10-08 17:18:54 +0200

    user middleware

--- rest/api/user/create.php
+++ rest/api/user/create.php
@@ -4,9 +4,7 @@ require_once __DIR__ . '/../header_post.php';
 require_once __DIR__ . '/../../config/Database.php';
 require_once __DIR__ . '/../../class/User.php';
 
-require_once __DIR__ . '/../../middleware/user.php';
-
-die;
+require_once __DIR__ . '/../../middleware/admin.php';
 
 $database = new Database();
 $db = $database->getConnection();
--- rest/api/user/update.php
+++ rest/api/user/update.php
@@ -6,19 +6,20 @@ require_once __DIR__ . '/../../class/User.php';
 
 require_once __DIR__ . '/../../middleware/user.php';
 
-die;
 
 $database = new Database();
 $db = $database->getConnection();
 
 $item = new User($db);
 
+$connectedUser = new User($db);
+$connectedUser->getConnectedUser();
+
 $data = json_decode(file_get_contents('php://input'));
 
-$item->email_user = $data->email_user;
+$item->email_user = $connectedUser->email_user;
 $item->pseudo_user = $data->pseudo_user;
 $item->password_user = hash('sha256', $data->password_user);
-$item->role_user = $data->role_user;
 
 if ($item->updateUser()) {
     echo json_encode('User updated successfully.');
