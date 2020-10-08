50a21a3696cd0bf34b3d618614cac850dfa6bf73
Noe Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2020-10-08 16:33:26 +0200

    album update api

---
+++ rest/api/album/update.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . '/../header_post.php';
+include_once __DIR__ . '/../../config/Database.php';
+include_once __DIR__ . '/../../class/Album.php';
+
+//require_once __DIR__ . '/../../middleware/user.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$item = new Album($db);
+
+$data = json_decode(file_get_contents('php://input'));
+
+$item->id = empty($data->id) ? die : $data->id;
+$item->artist_id = empty($data->artist_id) ? null : $data->artist_id;
+$item->title = empty($data->title) ? die : $data->title;
+$item->year = empty($data->year) ? null : $data->year;
+$item->image = empty($data->image) ? '' : $data->image;
+$item->tracks = empty($data->tracks) ? null : $data->tracks;
+
+
+if ($item->updateAlbum()) {
+    echo json_encode('Album updated successfully.');
+} else {
+    http_response_code(500);
+    echo json_encode('Album could not be updated.');
+}
