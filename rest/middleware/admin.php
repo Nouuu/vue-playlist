98bc1001669d0c3bcf5c33c118e7cfab63bf54ac
Noe Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2020-10-08 17:08:34 +0200

    admin middleware

---
+++ rest/middleware/admin.php
@@ -0,0 +1,34 @@
+<?php
+require_once __DIR__ . '/../class/User.php';
+require_once __DIR__ . '/../config/Database.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$item = new User($db);
+
+if (!$item->isConnected()) {
+    http_response_code(401);
+    echo json_encode(
+        ['message' => 'User not connected']
+    );
+    die;
+}
+$item->getConnectedUser();
+
+if (empty($item->email_user)) {
+    $item->logout();
+    http_response_code(401);
+    echo json_encode(
+        ['message' => 'Token expired']
+    );
+    die;
+}
+
+if ($item->role_user !== 'admin') {
+    http_response_code(403);
+    echo json_encode(
+        ['message' => 'User not allowed']
+    );
+    die;
+}
