1fadf6d3fcab8293d55c190bb312f895f439ff70
Noe Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2020-10-08 16:48:58 +0200

    read albums api

---
+++ rest/api/album/read.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__ . '/../header_get.php';
+
+require_once __DIR__ . '/../../config/Database.php';
+require_once __DIR__ . '/../../class/Album.php';
+
+//require_once __DIR__ . '/../../middleware/user.php';
+
+
+$database = new Database();
+$db = $database->getConnection();
+
+$items = new Album($db);
+
+$stmt = $items->getAlbums();
+$itemsCount = $stmt->rowCount();
+
+if ($itemsCount > 0) {
+    $albumArr = [];
+    $albumArr['body'] = [];
+    $albumArr['itemCount'] = $itemsCount;
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        array_push($albumArr['body'], Album::fromPDO($row, $db));
+    }
+    echo json_encode($albumArr);
+} else {
+    http_response_code(404);
+    echo json_encode(
+        ["message" => "No record found."]
+    );
+}
