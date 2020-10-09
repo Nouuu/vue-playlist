abeb223c0c1eef416cfc29dc426893905e4e1e69
Noe Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2020-10-09 16:09:43 +0200

    PDO

--- rest/class/AlbumInList.php
+++ rest/class/AlbumInList.php
@@ -27,12 +27,60 @@ class AlbumInList
 
     public function addAlbumInList()
     {
+        if ($this->exist()) {
+            return true;
+        }
 
+        $sql = 'insert into ' . $this->db_table . ' (album_id, list_id, grade, note)' .
+            ' VALUES (:album_id, :list_id, :grade, :note)';
+        $stmt = $this->conn->prepare($sql);
+
+        $this->title = htmlspecialchars(strip_tags($this->title));
+
+        $stmt->bindParam(':album_id', $this->album_id);
+        $stmt->bindParam(':list_id', $this->list_id);
+        $stmt->bindParam(':grade', $this->grade);
+        $stmt->bindParam(':note', $this->note);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
     }
 
     public function updateAlbumInList()
     {
+        $sql = 'update ' . $this->db_table . ' set ' .
+            'grade = :grade, ' .
+            'note = :note ' .
+            'where album_id = :album_id and list_id = :list_id';
+        $stmt = $this->conn->prepare($sql);
 
+        $this->note = htmlspecialchars(strip_tags($this->note));
+
+        $stmt->bindParam(':album_id', $this->album_id);
+        $stmt->bindParam(':list_id', $this->list_id);
+        $stmt->bindParam(':grade', $this->grade);
+        $stmt->bindParam(':note', $this->note);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function removeAlbumFromList()
+    {
+        $sql = 'delete from ' . $this->db_table . ' where album_id = :album_id and list_id = :list_id';
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bindParam(':album_id', $this->album_id);
+        $stmt->bindParam(':list_id', $this->list_id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
     }
 
     public function exist(): bool
