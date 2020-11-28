56fd76a134923d1199d1fda133f2f2f64671d350
Nouu <noelarrieulacoste@yahoo.fr>
2020-11-28 19:12:42 +0100

    env

---
+++ rest/env.example.php
@@ -0,0 +1,8 @@
+<?php
+define('env_database_type', '');
+define('env_database_name', '');
+define('env_db_server', '127.0.0.1');
+define('env_db_username', '');
+define('env_db_password', '');
+define('discogs_api_url', 'https://api.discogs.com');
+define('discogs_api_token', '');
--- rest/env.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('env_database_type', 'mysql');
-define('env_database_name', 'vue_mysql');
-define('env_db_server', '127.0.0.1');
-define('env_db_username', 'root');
-define('env_db_password', 'root');
-define('discogs_api_url', 'https://api.discogs.com');
-define('discogs_api_token', 'iYVWgqSIDEjmWvgMKZoMfrpESWtiAYJtXghMkhMM');
